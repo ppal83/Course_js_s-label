01fc2735f05e0fedcf8b76a9421a61bc670e5842
Ivan Simeyko <iv.simeyko@gmail.com>
2015-08-22 20:44:41 +0300

    День 05 - домашка: создал объект круг

--- project/day_05/js/day_05.js
+++ project/day_05/js/day_05.js
@@ -33,12 +33,18 @@ var app = function(arg) {
 };
 
 // Решение домашки
+background = function(color) {
+    if (color){
+        return ({background: color});
+    } else {
+        return ({background: 'black'});
+    }
+};
 
 function Square() {
     this.width = '100px';
     this.height = '100px';
-    this.background = 'grey';
-    this.color = 'white';
+    this.color = 'black';
     //this.text = 'This is, square!';
 
     this.render = function () {
@@ -48,16 +54,52 @@ function Square() {
         squareDiv.style.background = this.background;
         squareDiv.style.color = this.color;
        // squareDiv.innerHTML = this.color;
+        console.log('Выводим квадрат на страницу');
         return (document.body.appendChild(squareDiv))
     };
 
     this.move = function(move) {
-        move = 'left' || 'right' || 'up' || 'down'
+        move = 'left' || 'right' || 'up' || 'down';
         return move
     };
+
+    /*
+    this.changeColor = function(color) {
+        background.__proto__background = 'color';
+
+        console.log('Меняем цвет квадрата, должен пометься и цвет круга');
+        return (document.body.modifiers(this.render()));
+    }
+    */
 }
 
-var a = new Square();
+// Назначаем прототип
+Square.prototype = background;
+
+function Circle() {
+    this.width = '100px';
+    this.height = '100px';
+    /*-moz-border-radius: 25px;
+    -webkit-border-radius: 25px;*/
+    this.border = '50px';
+
+    this.render = function() {
+        var circleDiv = document.createElement('div')
+        circleDiv.style.width = this.width;
+        circleDiv.style.height = this.height;
+        circleDiv.style.background = this.background;
+        circleDiv.style.borderRadius = this.border;
+        console.log('Выводим круг на страницу');
+        return (document.body.appendChild(circleDiv))
+    }
+}
+
+//Назначаем прототип
+Circle.prototype = background;
+
+var square = new Square();
+var circle = new Circle();
+
 
 /*
 // Создаем объект в JS, который описывает в HTML квадрат
@@ -66,11 +108,10 @@ squareDiv.style.width = '100px';
 squareDiv.style.height = '100px';
 squareDiv.style.background = 'grey';
 squareDiv.style.color = 'white';
-squareDiv.innerHTML = 'Hello';
+squareDiv.innerHTML = 'Helllo';
 
 // Создаем объект в JS, который описывает в HTML круг
 // Этот вариант предпочтительней чем прошлый
 var circleDiv = document.createElement('div')   // создаем узел элемента
 circleDiv.className = 'circle';
 */
-
